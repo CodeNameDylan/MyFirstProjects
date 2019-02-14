1e0b1b30ef838e2fb8fea64dfac04039088667a7
CodeNameDylan <dsonen2@gmail.com>
2019-02-13 21:39:17 -0700

    Added median function

--- IO.py
+++ IO.py
@@ -5,6 +5,7 @@ Created on Feb 13, 2019
 This program takes multiple user inputs as a number, saves fart energy and outputs the nmbers entered, the sum of numbers,the highest and lowest entered, and the mean
 '''
 from sre_compile import isstring
+import statistics
 answered_int = True
 integersList = []
 while answered_int == True:
@@ -21,6 +22,7 @@ while answered_int == True:
                 print("Sum of entered numbers is: ", sum(integersList))
                 print("Highest Number: ", max(integersList) , "Lowest Number: ", min(integersList) )
                 print("The average of the input numbers is: ", sum(integersList)/len(integersList))
+                print("The median of the input numbers is: ", statistics.median(integersList))
                 break
             elif int(equations) == 2:
                 print("Thank you for trying my program")
